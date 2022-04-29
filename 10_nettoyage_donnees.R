1ca51d874370bd566fa3400b33f329c5e644f148
My Name <lucie.mc.tournier@gmail.com>
2022-04-29 22:30:30 +0200

    premier commit nettoyage des donnes

---
+++ 10_nettoyage_donnees.R
@@ -0,0 +1,20 @@
+## projet de serie temporelles 
+## serie etudiee : Indice brut de la production industrielle (base 100 en 2015) 
+##                  - Fabrication de glaces et sorbets (NAF rév. 2, niveau classe, poste 10.52)
+
+rm(list = ls())
+library(dplyr)
+
+RGPH_MLI <- read.csv( file = "data/valeurs_mensuelles.csv", header = TRUE, sep = ";")
+#, stringsAsFactors = TRUE 
+
+# creation de la base de donnees
+colnames(RGPH_MLI) <- c("date", "indice_production", "code")
+data <- select(RGPH_MLI, date,  indice_production)
+data <- data[-1,]
+data <- data[-1,]
+data <- data[-1,]
+
+data$indice_production <- as.numeric(data$indice_production)
+data$date <- paste(data$date,"01",sep="-")
+data$date <- as.Date(data$date)
